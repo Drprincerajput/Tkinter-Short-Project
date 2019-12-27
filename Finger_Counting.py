35ad289a58a9585c0b93d9a21c5662a243330a68
Drprincerajput <55732506+Drprincerajput@users.noreply.github.com>
2019-12-27 20:00:12 +0530

    Update Finger_Counting.py

--- Finger_Counting.py
+++ Finger_Counting.py
@@ -1,5 +1,6 @@
-#No By Fingre Counting 
-#COOL SUPER COOL
+#Count the given numbers on your fingers and find the correct finger on which the number ends.
+# The first number starts from the thumb, second on the index finger, third on the middle finger, fourth on the ring finger and fifth on the little finger.
+# Again six comes on the ring finger and so on.
 def num(n):
     r = n % 8
     if r == 1 :
